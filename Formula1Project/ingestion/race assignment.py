8a9410165edbb41f6e8a4754acb948e5b7f898f3
AmitGujar <AmitGujar>
2024-05-31 13:20:13 +0000

    refactored the notebook.

--- Formula1Project/ingestion/race assignment.py
+++ Formula1Project/ingestion/race assignment.py
@@ -11,7 +11,7 @@ race_schema = StructType(fields=[StructField("raceId", IntegerType(), False),
                                  StructField('circuitId', IntegerType(), True),
                                  StructField('name', StringType(), True),
                                  StructField('date', DateType(), True),
-                                 StructField('time', TimestampType(), True),
+                                 StructField('time', StringType(), True),
                                  StructField('url', StringType(), True)   
 ])
 # structypes will represent the rows and structfield will represent the columns
@@ -23,7 +23,6 @@ race_info=spark.read \
     .option('header', True) \
     .schema(race_schema) \
     .csv('/mnt/tfstorageisgreat53/raw/races.csv')
-display(race_info)
 # readting the file as per the schema
 
 # COMMAND ----------
@@ -40,22 +39,42 @@ race_data=race_info.select(col('raceId'),
                            col('name'),
                            col('date'),
                            col('time'))
-display(race_data)
 # filtering the columns except url
 
 # COMMAND ----------
 
 updated_race_data=race_data.withColumnRenamed('raceId', 'race_id') \
          .withColumnRenamed('circuitId', 'circuit_id') \
+         .withColumnRenamed('year', 'race_year') \
          .withColumnRenamed('name', 'tournament')
-display(updated_race_data)
 # updating the column headings
 
 # COMMAND ----------
 
-final_race_data=updated_race_data.withColumn('race_timestamp', to_timestamp(concat(col('date'), lit(''), col('time')), 'yyyy-MM-dd HH:mm:ss'))
-display(final_race_data)
+race_with_time = updated_race_data.withColumn('ingestion_date', current_timestamp()) \
+                                  .withColumn('race_timestamp', to_timestamp(concat(col('date'), lit(' '), col('time')), 'yyyy-MM-dd HH:mm:ss'))
 # adding the new column
+
+# COMMAND ----------
+
+final_race_data=race_with_time.select(col('race_id'),
+                                      col('race_year'),
+                                      col('round'),
+                                      col('circuit_id'),
+                                      col('tournament'),
+                                      col('ingestion_date'),
+                                      col('race_timestamp'))
+# filtering furthur colums from the table
+
+# COMMAND ----------
+
+updated_race_data.write.mode('overwrite').parquet('/mnt/tfstorageisgreat53/processed/race')
+# writing the updated data in parquet file format in adls
+
+# COMMAND ----------
+
+display(spark.read.parquet('/mnt/tfstorageisgreat53/processed/race'))
+# displaying the data from parquet file which is processed in adls
 
 # COMMAND ----------
 
