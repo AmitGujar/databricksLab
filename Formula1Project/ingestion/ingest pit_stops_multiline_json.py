a4d1135f846d28d46e2750a6ff618ebca5079ce7
AmitGujar <AmitGujar>
2024-06-12 04:30:02 +0000

    multiline file ingested

---
+++ Formula1Project/ingestion/ingest pit_stops_multiline_json.py
@@ -0,0 +1,48 @@
+# Databricks notebook source
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+# COMMAND ----------
+
+lap_times_schema = StructType(fields=[
+    StructField('raceId', IntegerType(), False),
+    StructField('driverId', IntegerType(), True),
+    StructField('lap', IntegerType(), True),
+    StructField('position', IntegerType(), True),
+    StructField('time', StringType(), True),
+    StructField('milliseconds', IntegerType(), True)
+])
+
+# COMMAND ----------
+
+laptimes_df = spark.read \
+    .schema(lap_times_schema) \
+    .csv('/mnt/tfstorageisgreat13/raw/lap_times')
+#    .csv('/mnt/tfstorageisgreat13/raw/lap_times/*.csv') this is another method 
+#use multiline is true if you are reading multiline json file
+
+# COMMAND ----------
+
+laptimes_final=laptimes_df.withColumnRenamed('raceId', 'race_id') \
+                        .withColumnRenamed('driverId', 'driver_id') \
+                        .withColumn('ingestion_date', current_timestamp())
+# in this we are renaming and adding ingestion date column with current timestamp
+
+
+# COMMAND ----------
+
+laptimes_final.write.mode('overwrite') \
+    .parquet('/mnt/tfstorageisgreat13/processed/laptimes')
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls /mnt/tfstorageisgreat13/processed
+
+# COMMAND ----------
+
+display(spark.read.parquet('/mnt/tfstorageisgreat13/processed/laptimes'))
+
+# COMMAND ----------
+
+
